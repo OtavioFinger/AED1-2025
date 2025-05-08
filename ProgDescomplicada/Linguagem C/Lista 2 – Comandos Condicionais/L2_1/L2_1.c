de7e9e1952e6d9b2b22cd0b697579207cdccb1f3
OtavioFinger <otaviofinger302@gmail.com>
2025-05-08 00:01:38 -0300

    atualização L2_1

--- ProgDescomplicada/Linguagem C/Lista 2 – Comandos Condicionais/L2_1/L2_1.c
+++ ProgDescomplicada/Linguagem C/Lista 2 – Comandos Condicionais/L2_1/L2_1.c
@@ -2,6 +2,18 @@
 #include <stdio.h>
 
 int main(){
-    
+    int a, b;
+
+    printf("Digite o primeiro número: ");
+    scanf("%d", &a);
+    printf("Digite o segundo número: ");
+    scanf("%d", &b);
+
+    if(a > b){
+        printf("O maior é %d\n", a);
+    }
+    else{
+        printf("O maior é %d\n", b);
+    }
     return 0;
 }
