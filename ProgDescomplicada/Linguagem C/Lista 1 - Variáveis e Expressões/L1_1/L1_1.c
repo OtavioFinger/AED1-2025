889df4ddc0a54a0c6519eaa794e07818e961ad99
OtavioFinger <otaviofinger302@gmail.com>
2025-05-07 23:53:14 -0300

    commit segundo prog descomplicada

--- ProgDescomplicada/Linguagem C/Lista 1 - Variáveis e Expressões/L1_1/L1_1.c
+++ ProgDescomplicada/Linguagem C/Lista 1 - Variáveis e Expressões/L1_1/L1_1.c
@@ -1,5 +1,7 @@
 //1. Fac¸a um programa que leia um n ´umero inteiro e o imprima.
 
+#include <stdio.h>
+
 int main(){
     int n = 3;
 
---
+++ ProgDescomplicada/Linguagem C/Lista 1 - Variáveis e Expressões/L1_2/L1_2.c
@@ -0,0 +1,13 @@
+//2. Fac¸a um programa que leia um n ´umero real e o imprima
+
+#include <stdio.h>
+
+int main(){
+
+    double n;
+
+    scanf("%lf", &n);
+    printf("Número: %lf\n", n);
+
+    return 0;
+}
