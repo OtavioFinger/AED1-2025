4d81f781a56767e6d69c0f61ce09d46880d6131c
OtavioFinger <otaviofinger302@gmail.com>
2025-05-20 23:31:20 -0300

    Primeiro Commit

---
+++ ProgDescomplicada/Linguagem C/Lista 10 – Alocação Dinâmica /L10_1/L10_1.c
@@ -0,0 +1,12 @@
+/*Crie um programa que:
+(a) Aloque dinamicamente um array de 5 n ´umeros inteiros,
+(b) Pec¸a para o usu ´ario digitar os 5 n ´umeros no espac¸o alocado,
+(c) Mostre na tela os 5 n ´umeros,
+(d) Libere a mem ´oria alocada */
+
+#include <stdio.h>
+
+int main () {
+
+    return 0;
+}
