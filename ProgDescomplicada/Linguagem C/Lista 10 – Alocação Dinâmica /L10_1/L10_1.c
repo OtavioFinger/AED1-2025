7ffd6cec9f4cba98565c0c5ce35768b3b60e63d8
OtavioFinger <otaviofinger302@gmail.com>
2025-05-20 23:54:07 -0300

    Segundo commit

--- ProgDescomplicada/Linguagem C/Lista 10 – Alocação Dinâmica /L10_1/L10_1.c
+++ ProgDescomplicada/Linguagem C/Lista 10 – Alocação Dinâmica /L10_1/L10_1.c
@@ -5,8 +5,26 @@
 (d) Libere a mem ´oria alocada */
 
 #include <stdio.h>
+#include <stdlib.h>
 
 int main () {
+    int *pVetor;
+    int i;
+
+    pVetor = ( int * ) malloc( 5 * sizeof(int) );
+
+        printf( "Digite 5 números inteiros: " );
+            for( i = 0; i < 5; i++ ) {
+                printf( "Número %d: ", i + 1 );
+                scanf( "%d", &pVetor[i] );
+            }
+            printf("\n");
+
+            printf("Você digitou:\n");
+            for (i = 0; i < 5; i++) {
+                printf("%d ", pVetor[i]);
+            }
+        free(pVetor);
 
     return 0;
 }
