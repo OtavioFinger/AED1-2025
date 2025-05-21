f37329b7deef3c0f11532e1beb89f6a221fd83ac
OtavioFinger <otaviofinger302@gmail.com>
2025-05-21 00:35:36 -0300

    Terceiro commit

--- ProgDescomplicada/Linguagem C/Lista 10 – Alocação Dinâmica /L10_3/L10_3.c
+++ ProgDescomplicada/Linguagem C/Lista 10 – Alocação Dinâmica /L10_3/L10_3.c
@@ -1,5 +1,6 @@
 //Fac¸a um programa que leia do usu ´ario o tamanho de um vetor a ser lido e fac¸a a alocac¸ ˜ao
-//din ˆamica de mem ´oria. Em seguida, leia do usu ´ario seus valores e imprima o vetor lido.
+//din ˆamica de mem ´oria. Em seguida, leia do usu ´ario seus valores e mostre quantos dos
+//n ´umeros s ˜ao pares e quantos s ˜ao ´ımpares
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -8,7 +9,7 @@ int main() {
 
     int *pVetor;
     int tamanhoVetor;
-    int i;
+    int i, pares = 0, impares = 0;
 
     printf( "Qual o tamanho do vetor? " );
         scanf( "%d", &tamanhoVetor );
@@ -19,13 +20,18 @@ int main() {
             for( i = 0; i < tamanhoVetor; i++ ) {
                 printf( "Número %d: ", i + 1 );
                 scanf( "%d", &pVetor[i] );
+
+                if ( pVetor[i] % 2 == 0)
+                    pares++;
+                else {
+                    impares++;
+                }
             }
             printf("\n");
 
-            printf("Você digitou:\n");
-            for (i = 0; i < tamanhoVetor; i++) {
-                printf("%d ", pVetor[i]);
-            }
+            printf( "Número de Impares: %d ", impares );
+            printf( "Número de Pares: %d ", pares );
+
         free(pVetor);
 
     return 0;
