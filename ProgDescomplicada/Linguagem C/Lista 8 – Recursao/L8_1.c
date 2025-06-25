d8091ced4ce36f2e51ecfed7d6c4ef46137ed5b1
OtavioFinger <otaviofinger302@gmail.com>
2025-06-24 22:32:02 -0300

    l8_1

---
+++ ProgDescomplicada/Linguagem C/Lista 8 – Recursao/L8_1.c
@@ -0,0 +1,30 @@
+// Crie uma func¸ ˜ao recursiva que receba um n ´umero inteiro positivo N e calcule o somat ´orio
+// dos n ´umeros de 1 a N.
+
+#include <stdio.h>
+
+int FuncaoSomatorio( int n );
+
+int main() {
+
+        int n = 0;
+    
+        printf("Digite o n para o somatório: ");
+        scanf("%d", &n);
+    
+        int resultado = FuncaoSomatorio(n);
+        printf("Somatório: %d\n", resultado);
+    
+        return 0;
+    }
+
+int FuncaoSomatorio( int n ) {
+ 
+    if( n == 0 ) {
+        return n;
+    } else if ( n == 1 ) {
+        return n;
+    } else {
+        return n + FuncaoSomatorio(n-1);
+    }
+}
