3f9b20e11018a348e1b79eb38bf8f11952c816de
OtavioFinger <otaviofinger302@gmail.com>
2025-06-25 13:49:08 -0300

    L_412 ou fizzbuzz

--- LeetCodes/1. Easy/L_412.c
+++ LeetCodes/1. Easy/L_412.c
@@ -1,30 +1,51 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 char** fizzBuzz(int n, int* returnSize);
 
 int main() {
+    int quantasRepeticoes, retornoValor;
+    char** matrizRespostas;
+
+
+    printf("Quantas vezes vai se repetir o jogo? ");
+        scanf("%d", &quantasRepeticoes);
+
+    matrizRespostas = fizzBuzz ( quantasRepeticoes, &retornoValor);
+
+    printf("\nResultado do FizzBuzz:\n");
+    for (int i = 0; i < quantasRepeticoes && retornoValor; i++) {
+        printf("%s\n", matrizRespostas[i]);
+    }
+
+    //COMO EU DESALOCO AS STRING DENTRO DE matrizRespostas?
+
+    free(matrizRespostas);
 
     return 0;
 }
+
 char** fizzBuzz(int n, int* returnSize) {
     
-    char** answer = (char **)malloc(n * sizeof(char*)); 
+    char **answer = (char **)malloc(sizeof(char*) * n);
     *returnSize = n;
-    
-    for (int i = 1; i <= n; i++) {
-        if (i % 3 == 0 && i % 5 == 0)
-            answer[i - 1] = "FizzBuzz";
-        else if (i % 3 == 0)
-            answer[i - 1] = "Fizz";
-        else if (i % 5 == 0)
-            answer[i - 1] = "Buzz";
-        else {
-            answer[i - 1] = malloc(12);
-            sprintf(answer[i - 1], "%d", i );
+
+    int verificador = 0;
+    for( int i = 0; i < n; i++ ) {
+        verificador = i + 1;
+
+        if ( verificador % 3 == 0 && verificador % 5 == 0 ) {
+            answer[i] = "FizzBuzz";
+        } else if ( verificador % 3 == 0 ) {
+            answer[i] = "Fizz";
+        } else if ( verificador % 5 == 0 ) {
+            answer[i] = "Buzz";
+        } else {
+            answer[i] = (char *)malloc(12); // 1 char é 1 byte, 11 bytes do int + \0, que é 1 char
+            sprintf(answer[i], "%d", verificador); //converte um int em "string"
         }
     }
     return answer;
-
 }
