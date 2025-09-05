5a11550099132971f190956e681cc6f8fbaf4760
Otávio Finger <otaviofinger302@gmail.com>
2025-09-04 23:57:04 -0300

    Isso ae

---
+++ ProgDescomplicada/Linguagem C/Lista 1 - Variáveis e Expressões/L1_24.c
@@ -0,0 +1,12 @@
+//Leia um valor de area em metros quadrados ´ m2 e apresente-o convertido em acres. A
+//formula de convers ´ ao˜ e: ´ A = M ∗ 0, 000247, sendo M a area em metros quadrados e ´ A
+//a area em acres.
+
+#include <stdio.h>
+
+int main() {
+
+    
+
+    return 0;
+}
